edca87dcd6f2de1acd5d55f342c38ce16afd7a4c
Peter Yoon <peyo.hd@gmail.com>
2016-12-18 13:43:50 +0900

    move gralloc.rpi to hardware/rpi/gralloc

--- gralloc_drm_priv.h
+++ gralloc_drm_priv.h
@@ -142,6 +142,14 @@ struct drm_module_t {
 	struct gralloc_drm_t *drm;
 };
 
+struct drm_gralloc1_module_t {
+    struct hw_module_t common;
+    int (*perform)(struct drm_gralloc1_module_t const* module,
+            int operation, ... );
+	pthread_mutex_t mutex;
+	struct gralloc_drm_t *drm;
+};
+
 struct gralloc_drm_drv_t {
 	/* destroy the driver */
 	void (*destroy)(struct gralloc_drm_drv_t *drv);
